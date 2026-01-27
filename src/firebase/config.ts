8a08271e3c8df9ad21a8d2ab8714ecb78d7e2584
RcConnell <rcconnell7@gmail.com>
2026-01-27 23:13:04 +0000

    File changes

--- src/firebase/config.ts
+++ src/firebase/config.ts
@@ -1,3 +1,10 @@
 // This file is automatically populated by the Firebase system.
 // Do not edit it manually.
-export const firebaseConfig = {};
+export const firebaseConfig = {
+  "projectId": "studio-7062771887-1161b",
+  "appId": "1:604389852678:web:40cc472b5131ef066ba587",
+  "apiKey": "AIzaSyBUt1b8kP42TqOh7HCliL6bxywI3A7gvk4",
+  "authDomain": "studio-7062771887-1161b.firebaseapp.com",
+  "measurementId": "",
+  "messagingSenderId": "604389852678"
+};
