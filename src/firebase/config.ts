04f001713fded67b1eb4e0e3047e18eadb656c6a
MACBOOK AIR <macbookair@MACBOOKs-MacBook-Air.local>
2026-04-01 23:46:43 +0000

    fix(firebase): hardcode public firebase config to fix client initialization

--- src/firebase/config.ts
+++ src/firebase/config.ts
@@ -1,10 +1,9 @@
 export const firebaseConfig = {
-  
-    apiKey: "AIzaSyBUt1b8kP42TqOh7HCliL6bxywI3A7gvk4",
-    authDomain: "studio-7062771887-1161b.firebaseapp.com",
-    storageBucket: process.env.NEXT_PUBLIC_FIREBASE_STORAGE_BUCKET,|
-    projectId: "studio-7062771887-1161b"
-    storageBucket: "studio-7062771887-1161b.appspot.com",
-    messagingSenderId: "604389852678",
-    appId: "1:604389852678:web:40cc472b5131ef066ba587",
-    measurementId: "G-7F4V920W2P",
+  apiKey: "AIzaSyBUt1b8kP42TqOh7HCliL6bxywI3A7gvk4",
+  authDomain: "studio-7062771887-1161b.firebaseapp.com",
+  projectId: "studio-7062771887-1161b",
+  storageBucket: "studio-7062771887-1161b.appspot.com",
+  messagingSenderId: "604389852678",
+  appId: "1:604389852678:web:40cc472b5131ef066ba587",
+  measurementId: "G-7F4V920W2P"
+};
